a7ae27f18c0b8989e4b28709e076545055cdcf21
Wagner Marçal <wagnermarcal.2008@gmail.com>
2022-01-19 15:17:49 -0300

    Initial files

---
+++ src/test/java/com/insiders/api/coursesplatform/CoursesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insiders.api.coursesplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
